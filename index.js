c80b846133f0ec02b4b6180dfa530cfe6d742f1b
ZenobiaZhang <38091965+ZenobiaZhang@users.noreply.github.com>
2019-02-22 11:13:22 -0500

    add para; understand promise

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+const rp = require('request-promise-native');
+const people_url = 'https://api.devhub.virginia.edu/v1/library/people';
+
 module.exports = {
-    test: function(){
-        console.log("Yuying is trying!");
+    test: function(agent, requestBody){
+        return rp.get(people_url).then(jsonBody => {
+            var body = JSON.Parse(jsonBody);
+            //if(requestBody.queryResult.parameters.people_service == "email") console.log("email")
+            let output = agent.add("people is developing");
+            return Promise.resolve(output);
+        });
     }
 }
