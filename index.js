b701b777274a2fe8e3874eaf706a10815d4279d1
ZenobiaZhang <38091965+ZenobiaZhang@users.noreply.github.com>
2019-03-07 15:02:14 -0500

    people function finished

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const rp = require('request-promise-native');
-const people_url = 'https://api.devhub.virginia.edu/v1/library/people';
+const people_url = "https://api.devhub.virginia.edu/v1/library/people";
+function test(agent, requestBody, url) {
+    return rp.get(url).then(jsonBody =>{
+        var body = JSON.parse(jsonBody);
+        console.log(body);
+        var docArray = body.response.docs;
+        //ask "who is technical person", more than 20 return
+        //find it according to jobTitle, return "body"(what they do)
+        //don't just return the first 20? Well yes you can bc they're most relevant
+        if(docArray.length>1) 
+            agent.add("There're at least"+docArray.length+"people who capable. Here're the most relevant ones:\n");
+        var result = "";
+        var num = 1;
+        for(var i = 0; i < docArray.length; i++) {
+            result = "" + num + ". " + docArray[i].displayName;
+            result += ": " + docArray[i].jobTitle + " at " + docArray[i].officeLocation + "\n";
+            result += "Email: " + docArray[i].email + "\n";
+            agent.add(result);
+            num++;
+            promiseRequest();//what does this do???
+        }
+        return Promise.resolve(agent);
+    });
+}
+
+function promiseRequest() {
+    console.log("hello");//what does this do???
+}
 
 module.exports = {
-    test: function(agent, requestBody){
-        return rp.get(people_url).then(jsonBody => {
-            var body = JSON.Parse(jsonBody);
-            //if(requestBody.queryResult.parameters.people_service == "email") console.log("email")
-            let output = agent.add("people is developing");
-            return Promise.resolve(output);
-        });
-    }
+    test: test
 }
