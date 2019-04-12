7c921e25d38e490b31dffe845f6971f5cdda961a
ZenobiaZhang <38091965+ZenobiaZhang@users.noreply.github.com>
2019-04-12 15:09:34 -0400

    comment format

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-// const rp = require('request-promise-native');
-// const people_url = "https://api.devhub.virginia.edu/v1/library/people";
-// function test(agent) {
-//     return rp.get(people_url).then(jsonBody =>{
-//         var body = JSON.parse(jsonBody);
-//         console.log(body);
-        
-//         var docArray = body.response.docs;
-//     });
-// }
-
+//Link rp to 
+//also include link
+//what it does
 const rp = require('request-promise-native');
 function test(agent, requestBody, url) {
 var hk = {
@@ -32,36 +24,9 @@ return rp(hk).then(function (people) {
         return Promise.resolve(agent);
 });
 }
-/*
-const rp = require('request-promise-native');
-function test(agent, requestBody, url) {
-    return rp.get(url).then(jsonBody =>{
-        var body = JSON.parse(jsonBody);
-        console.log(body);
-        var what_they_ask = requestBody.queryResult.parameters.people_service;
-        console.log(what_they_ask);
-        var talent = requestBody.queryResult.parameters.jobtitle;
-        for (var i=0; i<body.length; i++) {
-            if (body[i].jobTitle == talent) {
-                if (what_they_ask == "who") {
-                    result = "Name: " + body[i].displayName + "\n";
-                    result += "Email: "+body[i].email+"\n";
-                    result += "About: "+body[i].body+"\n"; 
-                    agent.add(result);
-                }
-                if (what_they_ask == "where") {
-                    result = "Name: "+body[i].displayName+"\n";
-                    result += "Location: "+body[i].officeLocation+"\n";
-                    agent.add(result);
-                }
-            }
-        }
-        return Promise.resolve(agent);
-    });
-}**/
 
 function promiseRequest() {
-    console.log("hello");//what does this do???
+    console.log("hello");
 }
 
 module.exports = {
